9b3302e9d136c5b48183ca48d1291b30d6332b67
Hans Rivera <hrivera@unal.edu.co>
2022-07-22 17:02:02 -0500

    added extended properties
    - added extended width
    - added extended height
    - added extended colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: "#F2EBE3",
+        "dark-cyan": "#3C8067",
+        "grayish-blue": "#6C7289",
+      },
+      width: {
+        110: "28.15rem",
+        128: "32rem",
+        132: "38rem",
+      },
+      height: {
+        110: "28.15rem",
+        128: "32rem",
+        132: "40rem",
+      },
+    },
   },
   plugins: [],
 };
